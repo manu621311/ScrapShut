f4c94a9626d91f618837e07ddafedfd2a6c8af36
Manumanglam <manurockmanglam@gmail.com>
2020-06-10 19:34:20 +0530

    final

--- scrapshut_project/settings.py
+++ scrapshut_project/settings.py
@@ -148,3 +148,6 @@ if ENVIRONMENT=='production':
     SESSION_COOKIE_SECURE=True
     CSRF_COOKIE_SECURE=True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # new
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
