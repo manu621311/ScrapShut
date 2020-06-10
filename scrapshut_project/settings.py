1624e70922c906c337acdcb88eced8cd6d93b076
Manumanglam <manurockmanglam@gmail.com>
2020-06-10 19:45:25 +0530

    final

--- scrapshut_project/settings.py
+++ scrapshut_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['scrapshuttask.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
